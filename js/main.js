119f46f34bf150a074e9d24ba5a734ba1a21dfa8
Sibin C Baby <41861279+sibincbaby@users.noreply.github.com>
2020-08-15 01:35:49 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function getLink(upiId) {
+	var name='sibi';
+	var bank='paytm';
+	var paymentStr = "upi://pay?pa=" + upiId +
+			"&pn=" + $('#pn').val() +
+			"&am=" + $('#am').val() +
+	    		"&tn=" + "#fingerTips";
+	return paymentStr;
+}
+
+
+
+function payToUpi() {
+	var upiId = $("#upi-input").val();
+	paymentStr = getLink(upiId);
+	window.location= paymentStr;
+}
+
+// upi://pay?pa=sruthisantho95@okaxis&pn=cek&tn=TreatmentFund&am=undefined
